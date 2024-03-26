c1fef0d4c7852dbca5c2ee80f99610d614241f56
Zaur Mamedov <zaurmd1996@gmail.com>
2024-03-26 21:11:56 +0300

    Add test 'test_open_login_form'

---
+++ tests/test_main_page.py
@@ -0,0 +1,23 @@
+from selene import browser, have
+
+
+def test_open_login_form():
+    browser.open('/')
+    browser.all('._25d45facb5--text--V2xLI').element_by(have.text('Войти')).click()
+    browser.element('._25d45facb5--title--OSQN1').should(have.text('Войти или\nзарегистрироваться'))
+
+
+def test_2():
+    pass
+
+
+def test_3():
+    pass
+
+
+def test_4():
+    pass
+
+
+def test_5():
+    pass
